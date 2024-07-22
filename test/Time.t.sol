87b6c7b3ebe58f90dd737d06c5786367c50d44c6
RutamBhagat <rutambhagat@gmail.com>
2024-07-22 21:31:05 +0530

    add wallet test

--- test/Time.t.sol
+++ test/Time.t.sol
@@ -17,4 +17,38 @@ contract TimeTest is Test {
         auction = new Auction();
         startAt = block.timestamp;
     }
+
+    function testBidFailsBeforeStartTime() public {
+        vm.expectRevert(bytes("cannot bid"));
+        auction.bid();
+    }
+
+    function testBid() public {
+        vm.warp(startAt + 1 days);
+        auction.bid();
+    }
+
+    function testBidFailsAfterEndTime() public {
+        vm.warp(startAt + 2 days);
+        vm.expectRevert(bytes("cannot bid"));
+        auction.bid();
+    }
+
+    function testTimestamp() public {
+        uint t = block.timestamp;
+
+        // skip - increment current timestamp
+        skip(100);
+        assertEq(block.timestamp, t + 100);
+
+        // rewind - decrement current timestamp
+        rewind(100);
+        assertEq(block.timestamp, t);
+    }
+
+    function testBlockNumber() public {
+        // vm.roll - set block.number
+        vm.roll(999);
+        assertEq(block.number, 999);
+    }
 }
