744d4776df5633ce9d343b062c8781c50016d1d0
RutamBhagat <rutambhagat@gmail.com>
2024-07-22 23:18:00 +0530

    add fork test

--- test/Fork.t.sol
+++ test/Fork.t.sol
@@ -1,7 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 import {Test, console} from "forge-std/Test.sol";
 
-contract ForkTest is Test {
-    function setUp() public {}
+interface IWETH {
+    function balanceOf(address) external view returns (uint256);
+    function deposit() external payable;
+}
 
-    function testWeth() public {}
+contract ForkTest is Test {
+    IWETH public weth;
+
+    function setUp() public {
+        weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    }
+
+    function testWeth() public {
+        uint balBefore = weth.balanceOf(address(this));
+        console.log("balance before", balBefore);
+
+        weth.deposit{value: 100}();
+        uint balAfter = weth.balanceOf(address(this));
+        console.log("balance after", balAfter);
+    }
 }
